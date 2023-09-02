ea81cc9ca1fada6b545cb3b916a5d67eae4f1bbe
Azim Baig <abdec2@hotmail.com>
2023-09-02 16:48:26 +0300

    changes uploaded

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+import path from 'path'
+ 
+
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "events", replacement: "rollup-plugin-node-polyfills/polyfills/events" },
+  
+    ],
   },
 });
